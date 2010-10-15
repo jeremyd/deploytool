a339d43c988cf0628e04bf61d92d2931cfa6853a
Jeremy Deininger <jeremy@rightscale.com>
2010-10-14 17:47:24 -0700

    adding spec, fixing up for release

--- lib/deploytool.rb
+++ lib/deploytool.rb
@@ -5,15 +5,6 @@ class Deploytool
     if `gem list|grep chef`.empty?
       STDERR.puts "WARNING: you don't appear to have chef installed"
     end
-    gempath = `gem env`.grep(/EXECUTABLE DIRECTORY/).first.split(/:/).last.chomp
-    @solo = File.join(gempath, "chef-solo")
-    @solo_config_path = File.join(File.expand_path("~"), "chef-solo")
-    FileUtils.mkdir_p(@solo_config_path)
-    solo_config =<<EOSOLO
-      file_cache_path "#{@solo_config_path}"
-      cookbook_path "#{@solo_config_path}/cookbooks"
-EOSOLO
-    File.open("#{@solo_config_path}/chef-solo.conf", "w") { |f| f.write solo_config }
   end
 
   #  Many customization options are available!!!
@@ -29,6 +20,13 @@ EOSOLO
     rev = "revision \"#{opts[:revision]}\"" if opts[:revision]
     action = ""
     action = "action :rollback" if opts[:rollback]
+    action = "action :force_deploy" if opts[:force]
+
+    # setup optional auth
+    suser = ""
+    suser = "svn_username #{ENV['OPT_SVN_USERNAME']}" if ENV['OPT_SVN_USERNAME']
+    spass = ""
+    spass = "svn_password #{ENV['OPT_SVN_USERNAME']}" if ENV['OPT_SVN_USERNAME']
 
     @cookbook =<<EOBOOK
       deploy_revision "#{opts[:destination]}" do
@@ -39,8 +37,8 @@ EOSOLO
         create_dirs_before_symlink Array.new
         purge_before_symlink Array.new
         scm_provider Chef::Provider::Subversion
-        svn_password "#{ENV['OPT_SVN_PASSWORD']}"
-        svn_username "#{ENV['OPT_SVN_USERNAME']}"
+        #{suser}
+        #{spass}
         #{action}
       end
 EOBOOK
@@ -59,6 +57,7 @@ EOBOOK
     rev = "revision \"#{opts[:revision]}\"" if opts[:revision]
     action = ""
     action = "action :rollback" if opts[:rollback]
+    action = "action :force_deploy" if opts[:force]
 
     @cookbook =<<EOBOOK
       deploy_revision "#{opts[:destination]}" do
@@ -76,6 +75,16 @@ EOBOOK
 
   # Run chef-solo; do the deploy.
   def run_solo
+    gempath = `gem env`.grep(/EXECUTABLE DIRECTORY/).first.split(/:/).last.chomp
+    @solo = File.join(gempath, "chef-solo")
+    @solo_config_path = File.join(File.expand_path("~"), "chef-solo")
+    FileUtils.mkdir_p(@solo_config_path)
+    solo_config =<<EOSOLO
+      file_cache_path "#{@solo_config_path}"
+      cookbook_path "#{@solo_config_path}/cookbooks"
+EOSOLO
+    File.open("#{@solo_config_path}/chef-solo.conf", "w") { |f| f.write solo_config }
+
     FileUtils.mkdir_p("#{@solo_config_path}/cookbooks/app_code/recipes")
     File.open("#{@solo_config_path}/cookbooks/app_code/recipes/deploy.rb", "w") { |f| f.write @cookbook }
     runlist = '{ "run_list": "app_code::deploy" }'
--- spec/deploytool_spec.rb
+++ spec/deploytool_spec.rb
@@ -1,7 +1,30 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'tmpdir'
+require 'fileutils'
 
 describe "Deploytool" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  before(:each) do
+    @dir = File.join(Dir.tmpdir, Time.now.strftime("%S%H%d"))
+    FileUtils.mkdir_p(@dir)
+  end
+  
+  it "runs a git deploy" do
+    opts = { :destination => @dir, :repository => "git://github.com/jeremyd/rest_connection.git", :revision => "master" }
+    deploy = Deploytool.new
+    deploy.use_git(opts)
+    deploy.run_solo
+    File.exists?(File.join(@dir, "current", "Rakefile")).should == true 
+  end
+
+  it "runs an svn deploy" do
+    opts = { :destination => @dir, :repository => "http://svn.apache.org/repos/asf/spamassassin/trunk" }
+    deploy = Deploytool.new
+    deploy.use_svn(opts)
+    deploy.run_solo
+    File.exists?(File.join(@dir, "current", "README")).should == true 
+  end
+
+  after(:each) do
+    FileUtils.rm_rf(@dir)
   end
 end
